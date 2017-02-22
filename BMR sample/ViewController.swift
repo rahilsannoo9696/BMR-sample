93d4e4055832a71251d2bda8c363800fc23f4930
Rahil <rahilkevin@icloud.com>
2017-02-22 10:32:10 +0530

    Initial Commit

---
+++ BMR sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BMR sample
+//
+//  Created by Rahil on 22/02/17.
+//  Copyright © 2017 sample. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
